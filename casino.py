c03091610061aafd51e382cf64e8bf7389462056
maxime <maxime@Phoenix>
2022-06-30 13:55:25 +0200

    v0.2

--- casino.py
+++ casino.py
@@ -1,4 +1,4 @@
-from operator import truediv
+#!/usr/bin/env python3.10
 from random import randrange
 from math import ceil
 
@@ -6,27 +6,26 @@ portefeuille=int(input("quelle est votre cagnotte: "))
     
 #tant que l'on a de l'argent
 while portefeuille>=1:
+#si erreur de saisie
     while True:
         mise=int(input("quelle est votre mise de départ: "))
         if (mise >1) and (mise <= portefeuille ) :
             break
         elif mise > portefeuille :
-            print("Vous n'avez pas la somme requise!!")   
+            print("Vous n'avez pas la somme requise, vous n'avez que", portefeuille, "€!!")   
         else:
-            print("erreur de saisie!")
+            print("erreur de saisie!mise minimum de 1€")
 
         
             
          
     x=randrange(50)
-    choix=int(input("Choissisez un numéro entre 0 et 49: "))
-    
-       
+    choix=int(input("Choissisez un numéro entre 0 et 49: "))    
     print("le numéro gangant est le ",x)
     
 #si gain
     if choix == x :
-        portefeuille= portefeuille - mise + (mise * 3)
+        portefeuille+= mise * 3
         print("Félicitations vous avez trouvé le bon nombre, vous avez gagné", mise*3,"€!")
         print("Il vous reste",portefeuille,"euros!" )
 #si bonne couleur
