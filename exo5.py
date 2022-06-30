ef0155c47ef19174935d29a0cc6a1d6b4a829c13
maxime <maxime@Phoenix>
2022-06-30 13:54:13 +0200

    exo5

---
+++ exo5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.10
+
+
+#on definie la fonction
+
+
+def somme(a,b):
+    c = a+b
+    return c
+    
+res = somme(1,2)
+print ("resultat: ", str(res))
+
+res = somme (4,4)
+print ("resultat: ", str(res))
+res = somme (-1, -1)
+print ("resultat: ", str(res))
+
